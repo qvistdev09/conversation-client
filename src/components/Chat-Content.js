a1e8e30d21700570ace863f3b54e2164c5238753
Oscar Lindqvist <olindqv@gmail.com>
2021-03-12 00:44:56 +0100

    style scrollbar

--- src/components/Chat-Content.js
+++ src/components/Chat-Content.js
@@ -3,16 +3,18 @@ import React from 'react';
 const ChatContent = ({ user, messageObj, children }) => {
   const formatTimeStamp = timeString => {
     const dateObj = new Date(timeString);
-    return dateObj.getHours() + ':' + (dateObj.getMinutes() < 10 ? `0${dateObj.getMinutes()}` : dateObj.getMinutes());
+    return (
+      (dateObj.getHours() < 10 ? `0${dateObj.getHours()}` : dateObj.getHours()) +
+      ':' +
+      (dateObj.getMinutes() < 10 ? `0${dateObj.getMinutes()}` : dateObj.getMinutes())
+    );
   };
 
   return (
     <div className='chat__content'>
-      <div className='chat__left'>
-        { children }
-      </div>
+      <div className='chat__left'>{children}</div>
       <div className='chat__right'>
-        <div className="chat__pointer"></div>
+        <div className='chat__pointer'></div>
         <div className='chat__content-header'>
           <p className='chat__nametag'>{user}</p>
           <p className='chat__timetag'>{formatTimeStamp(messageObj.time)}</p>
